73e1682f868d7370b0c417f6bb1a48099248cc78
ppp0 <philipp.gysin@cargomedia.ch>
2015-12-22 12:00:56 +0100

    elevate privileges for mkdir, cp

--- get-janus-headers.sh
+++ get-janus-headers.sh
@@ -5,5 +5,5 @@ source ./janus-version
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 git checkout ${JANUS_VERSION}
-mkdir -p /usr/include/janus
-cp {.,plugins}/*.h /usr/include/janus
+sudo mkdir -p /usr/include/janus
+sudo cp {.,plugins}/*.h /usr/include/janus
