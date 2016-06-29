e3db1358db8cd1a748ebfb58350a37a36d687ab0
Chris Stasiak <me@krzysztof-stasiak.com>
2016-06-29 21:20:16 +0100

    Removed duplicated condition statement

--- libjanus_rtpbroadcast.c
+++ libjanus_rtpbroadcast.c
@@ -2613,13 +2613,10 @@ static void *cm_rtpbcast_relay_thread(void *data) {
 							guint32 den = source->stats[j].max_seq_since_last_avg - source->stats[j].last_avg_seq;
 							/* If any packet received, let's check if all packets arrived based on sequence number */
 							if (den != 0 && source->stats[j].packets_since_last_avg < den) {
-								/* If any packet has been lost then let's assume the keyframe is corrupted */
-								if((den - source->stats[j].packets_since_last_avg) > 0) {
-									/* Mark the keyframe for current thumbnail record as corrupted */
-									mountpoint->trc[0]->had_keyframe = FALSE;
-									/* Force the restart of thumbnail record */
-									mountpoint->last_thumbnail = 0;
-								}
+								/* Mark the keyframe for current thumbnailing record as corrupted */
+								mountpoint->trc[0]->had_keyframe = FALSE;
+								/* Force the restart of thumbnailing record */
+								mountpoint->last_thumbnail = 0;
 							}
 							cm_rtpbcast_stop_thumbnailing(mountpoint, 0);
 						}
