2b6861517a877b8abc6598d04334e6bf0df0b8d6
Chris Stasiak <me@krzysztof-stasiak.com>
2016-09-08 15:25:35 +0200

    Updated comment

--- libjanus_rtpbroadcast.c
+++ libjanus_rtpbroadcast.c
@@ -2805,7 +2805,7 @@ static void cm_rtpbcast_relay_rtp_packet(gpointer data, gpointer user_data) {
 	}
 	if(!packet->is_keyframe && (!session->started || session->stopping || session->paused)) {
 		janus_mutex_unlock(&session->mutex);
-		//~ JANUS_LOG(LOG_INFO, "SKIPPED: Streaming not started yet for this session...\n");
+		//~ JANUS_LOG(LOG_ERR, "Streaming not started yet for this session...\n");
 		return;
 	}
 	if(session->destroyed > 0) {
